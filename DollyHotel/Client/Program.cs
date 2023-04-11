aab986c04910b07b6de7546e9afe905edf0253b9
Benjamin Fadina <omofadina@live.com>
2023-04-12 00:30:51 +0100

    First Commit

---
+++ DollyHotel/Client/Program.cs
@@ -0,0 +1,30 @@
+using DollyHotel.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+//builder.Services.AddHttpClient("DollyHotel.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+//    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<DollyHotel.Client.ConDataService>();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddHttpClient("DollyHotel.Server", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("DollyHotel.Server"));
+
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("DollyHotel.ServerAPI"));
+
+builder.Services.AddApiAuthorization();
+
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
+
+await builder.Build().RunAsync();
